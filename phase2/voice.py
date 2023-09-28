0bed884d681fa8c5b02499c4f32f6340b0b5ec9a
sanjeet kumar <sk0746309@gmail.com>
2023-09-28 11:43:20 +0530

    initial commit

--- phase2/voice.py
+++ phase2/voice.py
@@ -1,49 +1,88 @@
 import tkinter as tk
+from tkinter import ttk
 import sounddevice as sd
-import numpy as np
 import wave
 import os
-from tkinter import messagebox
 
-# Create a directory to store the recordings
-if not os.path.exists("recordings"):
-    os.mkdir("recordings")
+class VoiceRecorderApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Voice Recorder")
 
-recording_number = 1
-is_recording = False
+        self.recording = False
+        self.file_number = 1
+        # UI Elements
+        self.record_button = ttk.Button(root, text="Start Recording", command=self.toggle_recording)
+        self.record_button.pack(pady=20)
 
-def start_recording():
-    global is_recording
-    global recording_number
-    
-    if not is_recording:
-        is_recording = True
-        record_button.config(text="Stop Recording", bg="red")
-        file_name = f"recordings/recording{recording_number}.wav"
-        recording_number += 1
-        sd.default.samplerate = 44100  # Sample rate
-        sd.default.channels = 2  # Stereo
-        recording = sd.rec(int(10 * 44100), samplerate=44100, channels=2, dtype=np.int16)
+        self.file_listbox = tk.Listbox(root)
+        self.file_listbox.pack(pady=10)
+
+        self.save_button = ttk.Button(root, text="Save Recording", command=self.save_recording, state=tk.DISABLED)
+        self.save_button.pack()
+
+        # Create a directory to store the recordings
+        if not os.path.exists("recordings"):
+            os.mkdir("recordings")
+
+        self.refresh_file_list()
+
+    def toggle_recording(self):
+        if not self.recording:
+            self.start_recording()
+        else:
+            self.stop_recording()
+
+    def start_recording(self):
+        self.recording = True
+        self.record_button.config(text="Stop Recording")
+        self.save_button.config(state=tk.DISABLED)
+
+        # Start recording
+        self.recording_data = sd.rec(int(10 * 44100), samplerate=44100, channels=2, dtype='int16')
+
+    def stop_recording(self):
+        self.recording = False
+        self.record_button.config(text="Start Recording")
+        self.save_button.config(state=tk.NORMAL)
+
+        # Stop recording
         sd.wait()
+
+        # Save the recording as a WAV file
+        file_name = f"recordings/recording{self.file_number}.wav"
+        self.file_number += 1
+
         with wave.open(file_name, 'wb') as wf:
             wf.setnchannels(2)
             wf.setsampwidth(2)
             wf.setframerate(44100)
-            wf.writeframes(recording.tobytes())
-    else:
-        is_recording = False
-        record_button.config(text="Start Recording", bg="green")
+            wf.writeframes(self.recording_data.tobytes())
 
-def show_info():
-    messagebox.showinfo("About", "Voice Recorder Application\nVersion 1.0\n© 2023 Your Name")
+        self.refresh_file_list()
 
-app = tk.Tk()
-app.title("Voice Recorder")
+    def save_recording(self):
+        selected_item = self.file_listbox.curselection()
+        if selected_item:
+            selected_file = self.file_listbox.get(selected_item)
+            source_path = f"recordings/{selected_file}"
+            destination_path = f"saved_recordings/{selected_file}"
 
-record_button = tk.Button(app, text="Start Recording", command=start_recording, bg="green", padx=10, pady=5)
-record_button.pack(pady=20)
+            # Copy the recording to a new directory (e.g., 'saved_recordings')
+            os.makedirs("saved_recordings", exist_ok=True)
+            os.replace(source_path, destination_path)
 
-info_button = tk.Button(app, text="About", command=show_info)
-info_button.pack()
+            self.refresh_file_list()
 
-app.mainloop()
+    def refresh_file_list(self):
+        self.file_listbox.delete(0, tk.END)
+
+        # Display the list of recorded files
+        recordings = sorted(os.listdir("recordings"))
+        for recording in recordings:
+            self.file_listbox.insert(tk.END, recording)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = VoiceRecorderApp(root)
+    root.mainloop()
